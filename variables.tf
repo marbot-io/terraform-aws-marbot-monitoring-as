74954808c11d29bfb3998e377682520ff4e230fa
Michael Wittig <michael@widdix.de>
2023-05-08 21:00:31 +0200

    feat: support config via tags (#2)

--- variables.tf
+++ variables.tf
@@ -1,3 +1,22 @@
+# We can not only check the var.topic_arn !="" because of the Terraform error:  The "count" value depends on resource attributes that cannot be determined until apply, so Terraform cannot predict how many instances will be created.
+variable "create_topic" {
+  type        = bool
+  description = "Create SNS topic? If set to false you must set topic_arn as well!"
+  default     = true
+}
+
+variable "topic_arn" {
+  type        = string
+  description = "Optional SNS topic ARN if create_topic := false (usually the output of the modules marbot-monitoring-basic or marbot-standalone-topic)."
+  default     = ""
+}
+
+variable "stage" {
+  type        = string
+  description = "marbot stage (never change this!)."
+  default     = "v1"
+}
+
 variable "endpoint_id" {
   type        = string
   description = "Your marbot endpoint ID (to get this value: select a channel where marbot belongs to and send a message like this: \"@marbot show me my endpoint id\")."
@@ -26,45 +45,106 @@ variable "auto_scaling_group_name" {
   description = "The name of the Auto Scaling Group that you want to monitor."
 }
 
+
+
+variable "cpu_utilization" {
+  type        = string
+  description = "CPU utilization (static|off)."
+  default     = "static"
+}
+
 variable "cpu_utilization_threshold" {
   type        = number
-  description = "The maximum percentage of CPU utilization (set to -1 to disable)."
+  description = "The maximum percentage of CPU utilization (0-100)."
   default     = 80
+}
+
+variable "cpu_utilization_period" {
+  type        = number
+  description = "The period in seconds over which the specified statistic is applied (<= 86400 and multiple of 60)."
+  default     = 600
+}
+
+variable "cpu_utilization_evaluation_periods" {
+  type        = number
+  description = "The number of periods over which data is compared to the specified threshold (>= 1 and $period*$evaluation_periods <= 86400)."
+  default     = 1
+}
+
+
+
+variable "cpu_credit_balance" {
+  type        = string
+  description = "CPU burst credits for t* instances (static|off)."
+  default     = "static"
 }
 
 variable "cpu_credit_balance_threshold" {
   type        = number
-  description = "The minimum number of CPU credits available (t* instances only; set to -1 to disable)."
+  description = "The minimum number of CPU credits remaining in the burst bucket (>= 0)."
   default     = 20
+}
+
+variable "cpu_credit_balance_period" {
+  type        = number
+  description = "The period in seconds over which the specified statistic is applied (<= 86400 and multiple of 60)."
+  default     = 600
+}
+
+variable "cpu_credit_balance_evaluation_periods" {
+  type        = number
+  description = "The number of periods over which data is compared to the specified threshold (>= 1 and $period*$evaluation_periods <= 86400)."
+  default     = 1
+}
+
+
+
+variable "ebs_io_credit_balance" {
+  type        = string
+  description = "EBS I/O burst credits for smaller EBS optimized instance types (static|off)."
+  default     = "static"
 }
 
 variable "ebs_io_credit_balance_threshold" {
   type        = number
-  description = "The minimum percentage of I/O credits remaining in the burst bucket (smaller instance only; set to -1 to disable)."
+  description = "The minimum percentage of EBS I/O credits remaining in the burst bucket (0-100)."
   default     = 20
+}
+
+variable "ebs_io_credit_balance_period" {
+  type        = number
+  description = "The period in seconds over which the specified statistic is applied (<= 86400 and multiple of 60)."
+  default     = 600
+}
+
+variable "ebs_io_credit_balance_evaluation_periods" {
+  type        = number
+  description = "The number of periods over which data is compared to the specified threshold (>= 1 and $period*$evaluation_periods <= 86400)."
+  default     = 1
+}
+
+
+
+variable "ebs_throughput_credit_balance" {
+  type        = string
+  description = "EBS throughput burst credits for smaller EBS optimized instance types (static|off)."
+  default     = "static"
 }
 
 variable "ebs_throughput_credit_balance_threshold" {
   type        = number
-  description = "The minimum percentage of throughput credits remaining in the burst bucket (smaller instance only; set to -1 to disable)."
+  description = "The minimum percentage of EBS throughput credits remaining in the burst bucket (0-100)."
   default     = 20
 }
 
-# We can not only check the var.topic_arn !="" because of the Terraform error:  The "count" value depends on resource attributes that cannot be determined until apply, so Terraform cannot predict how many instances will be created.
-variable "create_topic" {
-  type        = bool
-  description = "Create SNS topic? If set to false you must set topic_arn as well!"
-  default     = true
+variable "ebs_throughput_credit_balance_period" {
+  type        = number
+  description = "The period in seconds over which the specified statistic is applied (<= 86400 and multiple of 60)."
+  default     = 600
 }
 
-variable "topic_arn" {
-  type        = string
-  description = "Optional SNS topic ARN if create_topic := false (usually the output of the modules marbot-monitoring-basic or marbot-standalone-topic)."
-  default     = ""
-}
-
-variable "stage" {
-  type        = string
-  description = "marbot stage (never change this!)."
-  default     = "v1"
+variable "ebs_throughput_credit_balance_evaluation_periods" {
+  type        = number
+  description = "The number of periods over which data is compared to the specified threshold (>= 1 and $period*$evaluation_periods <= 86400)."
+  default     = 1
 }
