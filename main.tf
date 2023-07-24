f0bff0f1df9e83fbd37b4c3390fef7e0bb4d83ee
Michael Wittig <michael@widdix.de>
2023-07-24 11:52:17 +0200

    chore: add static-analysis (#3)

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
 terraform {
   required_version = ">= 0.12.0"
   required_providers {
-    aws    = ">= 4.66.0"
-    random = ">= 2.2"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.66.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.2"
+    }
   }
 }
 
@@ -17,7 +23,7 @@ data "aws_autoscaling_group" "asg" {
 }
 
 locals {
-  topic_arn = var.create_topic == false ? var.topic_arn : join("", aws_sns_topic.marbot.*.arn)
+  topic_arn = var.create_topic == false ? var.topic_arn : join("", aws_sns_topic.marbot[*].arn)
   tags      = { for tag in data.aws_autoscaling_group.asg.tag : tag.key => tag.value }
   enabled   = var.enabled && lookup(local.tags, "marbot", "on") != "off"
 
@@ -68,7 +74,7 @@ resource "aws_sns_topic" "marbot" {
 resource "aws_sns_topic_policy" "marbot" {
   count = (var.create_topic && local.enabled) ? 1 : 0
 
-  arn    = join("", aws_sns_topic.marbot.*.arn)
+  arn    = join("", aws_sns_topic.marbot[*].arn)
   policy = data.aws_iam_policy_document.topic_policy.json
 }
 
@@ -77,7 +83,7 @@ data "aws_iam_policy_document" "topic_policy" {
     sid       = "Sid1"
     effect    = "Allow"
     actions   = ["sns:Publish"]
-    resources = [join("", aws_sns_topic.marbot.*.arn)]
+    resources = [join("", aws_sns_topic.marbot[*].arn)]
 
     principals {
       type = "Service"
@@ -91,7 +97,7 @@ data "aws_iam_policy_document" "topic_policy" {
     sid       = "Sid2"
     effect    = "Allow"
     actions   = ["sns:Publish"]
-    resources = [join("", aws_sns_topic.marbot.*.arn)]
+    resources = [join("", aws_sns_topic.marbot[*].arn)]
 
     principals {
       type        = "AWS"
@@ -110,7 +116,7 @@ resource "aws_sns_topic_subscription" "marbot" {
   depends_on = [aws_sns_topic_policy.marbot]
   count      = (var.create_topic && local.enabled) ? 1 : 0
 
-  topic_arn              = join("", aws_sns_topic.marbot.*.arn)
+  topic_arn              = join("", aws_sns_topic.marbot[*].arn)
   protocol               = "https"
   endpoint               = "https://api.marbot.io/${var.stage}/endpoint/${var.endpoint_id}"
   endpoint_auto_confirms = true
@@ -145,7 +151,7 @@ resource "aws_cloudwatch_event_rule" "monitoring_jump_start_connection" {
 resource "aws_cloudwatch_event_target" "monitoring_jump_start_connection" {
   count = (var.module_version_monitoring_enabled && local.enabled) ? 1 : 0
 
-  rule      = join("", aws_cloudwatch_event_rule.monitoring_jump_start_connection.*.name)
+  rule      = join("", aws_cloudwatch_event_rule.monitoring_jump_start_connection[*].name)
   target_id = "marbot"
   arn       = local.topic_arn
   input     = <<JSON
@@ -301,7 +307,7 @@ JSON
 resource "aws_cloudwatch_event_target" "unsuccessful" {
   count = local.enabled ? 1 : 0
 
-  rule      = join("", aws_cloudwatch_event_rule.unsuccessful.*.name)
+  rule      = join("", aws_cloudwatch_event_rule.unsuccessful[*].name)
   target_id = "marbot"
   arn       = local.topic_arn
 }
