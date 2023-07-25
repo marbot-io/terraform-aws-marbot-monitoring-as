a89cd88a130520e3d84443a1bfe0d17db6d7e8d9
Michael Wittig <michael@widdix.de>
2023-07-25 13:49:16 +0200

    chore: add acceptance tests

---
+++ examples/default/variables.tf
@@ -0,0 +1,8 @@
+variable "endpoint_id" {
+  type        = string
+  description = "Your marbot endpoint ID (to get this value: select a channel where marbot belongs to and send a message like this: \"@marbot show me my endpoint id\")."
+}
+
+variable "auto_scaling_group_name" {
+  type        = string
+}
